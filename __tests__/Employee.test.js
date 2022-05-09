e7779136a6f40ad9325e41044c35270f47b5e847
unknown <williambrooks287@gmail.com>
2022-05-09 15:47:13 -0400

    add employee class and tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,3 +8,27 @@ test('create employee object', () => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
+
+test('check getName method', () => {
+    const employee = new Employee('Will', 20, 'will@email.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('check getId method', () => {
+    const employee = new Employee('Will', 20, 'will@email.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test('check getEmail method', () => {
+    const employee = new Employee('Will', 20, 'will@email.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('check getRole method', () => {
+    const employee = new Employee('Will', 20, 'will@email.com');
+
+    expect(employee.getRole()).toBe('Employee');
+});
